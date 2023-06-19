ac77c708bf587071db74a56580aa7e99c05f0222
Sami Uski <ad1815@student.jamk.fi>
2023-06-19 12:46:29 +0300

    update commit
    SceneManager for Final scene, to be able to change scene's back to start.

---
+++ Assets/Scripts/Sami/ChangeSceneScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeSceneScript : MonoBehaviour
+{
+    [SerializeField] Button changeSceneButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeSceneButton.onClick.AddListener(() => ChangeScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("Stage1 Copy");
+    }
+}
